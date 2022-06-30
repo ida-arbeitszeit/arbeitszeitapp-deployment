dc7952c65c066f6437989bf61c36f408e06f718b
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-06-30 20:03:10 +0200

    Use nixos-22.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
   description = "A very basic flake";
-  inputs = { arbeitszeitapp.url = "github:arbeitszeit/arbeitszeitapp"; };
+  inputs = {
+    arbeitszeitapp.url = "github:arbeitszeit/arbeitszeitapp";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  };
 
   outputs = { self, nixpkgs, arbeitszeitapp }:
     let
