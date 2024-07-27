a1aa1094ec7cdc93911e1086310a9d93fb4f286b
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2024-07-27 15:34:53 +0200

    Use nixfmt-rfc-style for auto code formatting

--- flake.nix
+++ flake.nix
@@ -8,60 +8,77 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs-23-11, nixpkgs-24-05, nixpkgs-unstable
-    , arbeitszeitapp, flake-utils }:
+  outputs =
+    {
+      self,
+      nixpkgs-23-11,
+      nixpkgs-24-05,
+      nixpkgs-unstable,
+      arbeitszeitapp,
+      flake-utils,
+    }:
     let
-      systemDependent = flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      systemDependent = flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+        system:
         let
           pkgs-unstable = import nixpkgs-unstable { inherit system; };
-          makeSimpleTest = testFile: name: nixpkgs:
+          makeSimpleTest =
+            testFile: name: nixpkgs:
             nixpkgs.nixosTest {
               name = name;
-              nodes.machine = { config, ... }: {
-                imports = [ self.nixosModules.default ];
-                nixpkgs.pkgs = nixpkgs;
-                services.arbeitszeitapp.enable = true;
-                services.arbeitszeitapp.hostName = "localhost";
-                services.arbeitszeitapp.enableHttps = false;
-                services.arbeitszeitapp.emailEncryptionType = null;
-                services.arbeitszeitapp.emailConfigurationFile =
-                  nixpkgs.writeText "mailconfig.json" (builtins.toJSON {
-                    MAIL_SERVER = "mail.server.example";
-                    MAIL_PORT = "465";
-                    MAIL_USERNAME = "mail@mail.server.example";
-                    MAIL_PASSWORD = "secret password";
-                    MAIL_DEFAULT_SENDER = "sender@mail.server.example";
-                  });
-              };
+              nodes.machine =
+                { config, ... }:
+                {
+                  imports = [ self.nixosModules.default ];
+                  nixpkgs.pkgs = nixpkgs;
+                  services.arbeitszeitapp.enable = true;
+                  services.arbeitszeitapp.hostName = "localhost";
+                  services.arbeitszeitapp.enableHttps = false;
+                  services.arbeitszeitapp.emailEncryptionType = null;
+                  services.arbeitszeitapp.emailConfigurationFile = nixpkgs.writeText "mailconfig.json" (
+                    builtins.toJSON {
+                      MAIL_SERVER = "mail.server.example";
+                      MAIL_PORT = "465";
+                      MAIL_USERNAME = "mail@mail.server.example";
+                      MAIL_PASSWORD = "secret password";
+                      MAIL_DEFAULT_SENDER = "sender@mail.server.example";
+                    }
+                  );
+                };
               testScript = builtins.readFile testFile;
             };
-          makeTestWithProfiling = testFile: name: nixpkgs:
+          makeTestWithProfiling =
+            testFile: name: nixpkgs:
             nixpkgs.nixosTest {
               name = name;
-              nodes.machine = { config, ... }: {
-                virtualisation.memorySize = 2048;
-                virtualisation.diskSize = 1024;
-                imports = [ self.nixosModules.default ];
-                nixpkgs.pkgs = nixpkgs;
-                services.arbeitszeitapp.enable = true;
-                services.arbeitszeitapp.hostName = "localhost";
-                services.arbeitszeitapp.enableHttps = false;
-                services.arbeitszeitapp.emailEncryptionType = null;
-                services.arbeitszeitapp.emailConfigurationFile =
-                  nixpkgs.writeText "mailconfig.json" (builtins.toJSON {
-                    MAIL_SERVER = "mail.server.example";
-                    MAIL_PORT = "465";
-                    MAIL_USERNAME = "mail@mail.server.example";
-                    MAIL_PASSWORD = "secret password";
-                    MAIL_DEFAULT_SENDER = "sender@mail.server.example";
-                  });
-                services.arbeitszeitapp.profilingEnabled = true;
-                services.arbeitszeitapp.profilingCredentialsFile =
-                  nixpkgs.writeText "profiling.json" (builtins.toJSON {
-                    PROFILING_AUTH_USER = "testuser";
-                    PROFILING_AUTH_PASSWORD = "testpassword";
-                  });
-              };
+              nodes.machine =
+                { config, ... }:
+                {
+                  virtualisation.memorySize = 2048;
+                  virtualisation.diskSize = 1024;
+                  imports = [ self.nixosModules.default ];
+                  nixpkgs.pkgs = nixpkgs;
+                  services.arbeitszeitapp.enable = true;
+                  services.arbeitszeitapp.hostName = "localhost";
+                  services.arbeitszeitapp.enableHttps = false;
+                  services.arbeitszeitapp.emailEncryptionType = null;
+                  services.arbeitszeitapp.emailConfigurationFile = nixpkgs.writeText "mailconfig.json" (
+                    builtins.toJSON {
+                      MAIL_SERVER = "mail.server.example";
+                      MAIL_PORT = "465";
+                      MAIL_USERNAME = "mail@mail.server.example";
+                      MAIL_PASSWORD = "secret password";
+                      MAIL_DEFAULT_SENDER = "sender@mail.server.example";
+                    }
+                  );
+                  services.arbeitszeitapp.profilingEnabled = true;
+                  services.arbeitszeitapp.profilingCredentialsFile = nixpkgs.writeText "profiling.json" (
+                    builtins.toJSON {
+                      PROFILING_AUTH_USER = "testuser";
+                      PROFILING_AUTH_PASSWORD = "testpassword";
+                    }
+                  );
+                };
               testScript = builtins.readFile testFile;
             };
           nixpkgsVersions = {
@@ -69,40 +86,55 @@
             nixpkgs-24-05 = import nixpkgs-24-05 { inherit system; };
             nixpkgs-unstable = import nixpkgs-unstable { inherit system; };
           };
-          makeTestMatrix = testFunctions:
-            builtins.foldl' (tests: testName:
-              tests // (makeTests testFunctions."${testName}" testName
-                (builtins.attrNames nixpkgsVersions))) { }
-            (builtins.attrNames testFunctions);
-          makeTests = testFunction: testName:
-            builtins.foldl' (tests: nixpkgsName:
-              tests // {
+          makeTestMatrix =
+            testFunctions:
+            builtins.foldl' (
+              tests: testName:
+              tests // (makeTests testFunctions."${testName}" testName (builtins.attrNames nixpkgsVersions))
+            ) { } (builtins.attrNames testFunctions);
+          makeTests =
+            testFunction: testName:
+            builtins.foldl' (
+              tests: nixpkgsName:
+              tests
+              // {
                 "${testName}-${nixpkgsName}" =
                   testFunction "${testName}-${nixpkgsName}"
-                  nixpkgsVersions."${nixpkgsName}";
-              }) { };
+                    nixpkgsVersions."${nixpkgsName}";
+              }
+            ) { };
           testCases = {
             lauchWebserver = makeSimpleTest tests/launchWebserver.py;
-            launchWebserverWithProfiler =
-              makeTestWithProfiling tests/launchWebserver.py;
+            launchWebserverWithProfiler = makeTestWithProfiling tests/launchWebserver.py;
             testProfiling = makeTestWithProfiling tests/testProfiling.py;
           };
-          pythonEnv = pkgs-unstable.python3.withPackages
-            (p: with p; [ black mypy flake8 isort ]);
-        in {
+          pythonEnv = pkgs-unstable.python3.withPackages (
+            p: with p; [
+              black
+              mypy
+              flake8
+              isort
+            ]
+          );
+        in
+        {
           devShells = {
             default = pkgs-unstable.mkShell {
-              packages = [ pythonEnv pkgs-unstable.nixfmt pkgs-unstable.gh ];
+              packages = [
+                pythonEnv
+                pkgs-unstable.nixfmt-rfc-style
+                pkgs-unstable.gh
+              ];
             };
           };
           checks = makeTestMatrix testCases;
-        });
+        }
+      );
       systemIndependent = {
         nixosModules = {
-          default = import modules/default.nix {
-            overlay = arbeitszeitapp.overlays.default;
-          };
+          default = import modules/default.nix { overlay = arbeitszeitapp.overlays.default; };
         };
       };
-    in systemDependent // systemIndependent;
+    in
+    systemDependent // systemIndependent;
 }
